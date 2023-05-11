1897954fe8bab257eed45ddc670c71a4da78c0c8
sabuj0143 <sabujhowlader011@gmai.com>
2023-05-11 11:09:46 +0600

    Booking data added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 
@@ -30,10 +30,32 @@ async function run() {
         await client.connect();
 
         const serviceCollection = client.db('carDoctor').collection('services');
+        const bookingCollection = client.db('carDoctor').collection('bookings');
 
-        app.get('/services', async(req, res) => {
+        // All Data Load..
+        app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
+            res.send(result);
+        })
+        // Single Data Load..
+        app.get('/services/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+
+            const options = {
+                // Include only the `title` and `price` fields in the returned document
+                projection: { title: 1, price: 1, service_id:1 },
+              };
+
+            const result = await serviceCollection.findOne(query, options);
+            res.send(result);
+        })
+        // Client site Bookings service Load the server site
+        app.post('/bookings', async(req, res) => {
+            const booking = req.body;
+            console.log(booking);
+            const result = await bookingCollection.insertOne(booking);
             res.send(result);
         })
 
