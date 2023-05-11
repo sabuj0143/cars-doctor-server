7fe2b5c53577d4885579e1ac2ec92c17ec1ec372
sabuj0143 <sabujhowlader011@gmai.com>
2023-05-11 11:42:56 +0600

    More added

--- index.js
+++ index.js
@@ -39,20 +39,30 @@ async function run() {
             res.send(result);
         })
         // Single Data Load..
-        app.get('/services/:id', async(req, res) => {
+        app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
 
             const options = {
                 // Include only the `title` and `price` fields in the returned document
-                projection: { title: 1, price: 1, service_id:1 },
-              };
+                projection: { title: 1, price: 1, service_id: 1, img: 1 },
+            };
 
             const result = await serviceCollection.findOne(query, options);
             res.send(result);
         })
         // Client site Bookings service Load the server site
-        app.post('/bookings', async(req, res) => {
+        app.get('/bookings', async (req, res) => {
+            console.log(req.query.email);
+            let query = {};
+            if (req.query?.email) {
+                const query = { email: req.query.email }
+            }
+            const result = await bookingCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.post('/bookings', async (req, res) => {
             const booking = req.body;
             console.log(booking);
             const result = await bookingCollection.insertOne(booking);
